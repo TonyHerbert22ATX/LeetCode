4303e4c0b1ec90b929c27c3afa8132c8770382b4
Tony Herbert <tonyherbert22@gmail.com>
2022-11-01 12:10:31 -0500

    daily Leetcode

---
+++ All Solutions/26.RemoveDuplicatesFromSortedArray.js
@@ -0,0 +1,18 @@
+// array of #... 1 -> infinite....
+
+// remove dups 
+
+
+let removeDuplicates = function(nums) {
+    let counter = 0
+    for ( let i = 0; i < nums.length; i++){
+        if(nums[i] != nums[i-1]){
+            nums[counter] = nums[i]
+            counter++
+        }
+    }
+    return counter
+};
+
+
+console.log(removeDuplicates([1,1,2]))
